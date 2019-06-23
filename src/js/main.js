90d5c77b2e6b09b2b847a9766ad9c5775786902b
matheus solidade <mhsolidade@gmail.com>
2019-06-23 16:55:48 -0300

    add change nav header

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 $(function(){
+    $('#metodologia').load('src/pages/metodologia.html')
     
 })
 
 $(document).ready(function() {
-    $('#metodologia').load('src/pages/metodologia.html')
     let last_known_scroll_position = 0;
     let ticking = false;
-    
     function doSomething(scroll_pos) {
-        console.log(scroll_pos)
         if(scroll_pos > 10 ) {
-            $('#navheader').removeClass('navheader-color')
-        }else{
+            
+            $('#navheader').removeClass('navheader-color', 'bg-dark')
             $('#navheader').addClass('navheader-color')
+        }else{
+            $('#navheader').removeClass('navheader-color', 'bg-dark')
+            $('#navheader').addClass('bg-dark')
 
         }
     }
