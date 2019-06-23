7ee30068b3b14acbbfe160d45e45916d16f822d1
matheus solidade <mhsolidade@gmail.com>
2019-06-23 18:29:09 -0300

    add tbs

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#metodologia').load('src/pages/tabelas/metodologia.html')
+    $('#metodologia').load('src/pages/tabelas/Quadro38.html')
     
 })
 
@@ -7,15 +7,15 @@ $(document).ready(function() {
     let last_known_scroll_position = 0;
     let ticking = false;
     function doSomething(scroll_pos) {
-        if(scroll_pos > 10 ) {
-            
-            $('#navheader').removeClass('navheader-color', 'bg-dark')
-            $('#navheader').addClass('navheader-color')
-        }else{
-            $('#navheader').removeClass('navheader-color', 'bg-dark')
-            $('#navheader').addClass('bg-dark')
+      if(scroll_pos > 10 ) {
+          
+        $('#navheader').removeClass('navheader-color', 'bg-dark')
+        $('#navheader').addClass('navheader-color')
+      }else{
+        $('#navheader').removeClass('navheader-color', 'bg-dark')
+        $('#navheader').addClass('bg-dark')
 
-        }
+      }
     }
     
     window.addEventListener('scroll', function(e) {
