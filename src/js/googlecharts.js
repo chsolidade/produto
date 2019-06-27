029c6828e0f384aa88ab1bb76f0c7360d513b6e0
matheus solidade <mhsolidade@gmail.com>
2019-06-27 00:31:39 -0300

    remove css

--- src/js/googlecharts.js
+++ src/js/googlecharts.js
@@ -1,5 +1,5 @@
 google.charts.load('current', {
-  'packages':['geochart','bar','corechart'],
+  'packages':['geochart','bar','corechart', 'line'],
   // Note: you will need to get a mapsApiKey for your project.
   // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
   'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
